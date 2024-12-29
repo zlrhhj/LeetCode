81bee3772d8891e9a7d67449c637c8d5143ff759
Liru Zhao <zlrhhj@hotmail.com>
2024-12-29 10:27:20 -0800

    No.91 Decode Ways ---dynamic programming

--- src/DecodeWays.java
+++ src/DecodeWays.java
@@ -1,7 +1,32 @@
 public class DecodeWays {
     private int count;
-
     public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        if(s.charAt(0) == '0') {
+            return 0;
+        } else {
+            dp[0] = 1;
+        }
+        if(s.length() == 1) {
+            return dp[0];
+        }
+        if(s.charAt(1) != '0') {
+            dp[1] = dp[0];
+        }
+        if(Integer.parseInt(s.substring(0, 2)) <= 26) {
+            dp[1]++;
+        }
+        for(int i = 2; i < s.length(); i++) {
+            if(s.charAt(i) != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if(s.charAt(i - 1) != '0' && Integer.parseInt(s.substring(i - 1, i + 1)) <= 26) {
+                dp[i] = dp[i] + dp[i - 2];
+            }
+        }
+        return dp[s.length() - 1];
+    }
+    public int numDecodings1(String s) {
         count = 0;
         backTrack(s, 0);
         return count;
