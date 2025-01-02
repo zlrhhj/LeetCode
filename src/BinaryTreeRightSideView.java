e8e30afa5dc7df5bba89b976ba4548bedbaabc83
Liru Zhao <zlrhhj@hotmail.com>
2025-01-02 09:55:32 -0800

    No.199 Binary Tree Right Side View

---
+++ src/BinaryTreeRightSideView.java
@@ -0,0 +1,40 @@
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){};
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class BinaryTreeRightSideView {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        helper(root, 0);
+        return result;
+    }
+    public void helper(TreeNode node, int level) {
+       if(result.size() == level) {
+           rsult.add(node.val);
+       }
+       if(node.right != null) {
+           helper(node.right, level + 1);
+       }
+       if(node.left != null) {
+           helper(node.left, level + 1);
+       }
+    }
+}
