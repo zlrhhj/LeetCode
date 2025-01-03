ce8a041391921a470f608d510aca5ca8c8b19069
Liru Zhao <zlrhhj@hotmail.com>
2025-01-02 16:18:37 -0800

    No.206 Reverse Linked list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){};
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val,ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 public class ReverseLinkedList {
+    public ListNode reverserList(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+        ListNode curNode = head;
+        ListNode preNode = null;
+        while(curNode != null) {
+            ListNode nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        return preNode;
+    }
 }
