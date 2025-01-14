6845409d501023e426992246ccc7456be0f714fa
Liru Zhao <zlrhhj@hotmail.com>
2025-01-13 19:05:57 -0800

    a new solution for NO.739 Daily Temperatures

--- src/DailyTemperatures.java
+++ src/DailyTemperatures.java
@@ -1,6 +1,28 @@
 import java.util.Stack;
 
 public class DailyTemperatures {
+    //using reverse iterating, Space Complexity is O(1)
+    public int[] dailyTemperatures1(int[] temperatures) {
+        int len = temperatures.length;
+        int answer[] = new int[len];
+        int hottest = 0;
+
+        for(int i = len - 1; i >= 0; i--) {
+            int currentDay = i;
+            int currentTemp = temperatures[currentDay];
+            if(currentTemp >= hottest) {
+                hottest = currentTemp;
+                continue;
+            }
+            int days = 1;
+            while(temperatures[currentDay + days] <= currentTemp ) {
+                days += answer[currentDay + days];
+            }
+            answer[currentDay] = days;
+        }
+        return answer;
+    }
+    //using Stack Space Complexity is O(N)
     public int[] dailyTemperatures(int[] temperatures) {
         Stack<int[]> stack = new Stack<>();
         int len = temperatures.length;
