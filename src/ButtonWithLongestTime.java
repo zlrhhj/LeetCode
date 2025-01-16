ef9a4ba69339ca815aa031c2c7eaf27f3e54e20c
Liru Zhao <zlrhhj@hotmail.com>
2025-01-15 17:43:00 -0800

    No.3386 button with longest push time

--- src/ButtonWithLongestTime.java
+++ src/ButtonWithLongestTime.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class ButtonWithLongestTime {
+    public int buttonWithLongestTime(int[][] events) {
+        int longestTime = events[0][1];
+        int result = events[0][0];
+        for(int i = 1; i < events.length; i++) {
+            int time = events[i][1] - events[i - 1][1];
+            if(time > longestTime) {
+                longestTime = time;
+                result = events[i][0];
+            } else if(time == longestTime) {
+                result = Math.min(result, events[i][0]);
+            }
+        }
+        return result;
+    }
 }
