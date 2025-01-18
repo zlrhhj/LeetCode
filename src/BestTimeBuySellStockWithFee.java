22142eb40c5753c1dfc978fd3674c8ba92285fe9
Liru Zhao <zlrhhj@hotmail.com>
2025-01-18 11:23:51 -0800

    Attempt #2 for No.714, with better space complexity

--- src/BestTimeBuySellStockWithFee.java
+++ src/BestTimeBuySellStockWithFee.java
@@ -1,4 +1,5 @@
 public class BestTimeBuySellStockWithFee {
+    //Attempt #1 dynamic programming
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
         int[] hold = new int[n];
@@ -9,5 +10,17 @@ public class BestTimeBuySellStockWithFee {
             free[i] = Math.max(free[i - 1], hold[i - 1] + prices[i] - fee);
         }
         return free[n - 1];
+    }
+
+    //Attemp #2  dynamic programming with less Space complexity
+    public int maxProfit2(int[] prices, int fee) {
+        int n = prices.length;
+        int hold = -prices[0];
+        int free = 0;
+        for(int i = 1; i < n; i++) {
+            hold = Math.max(hold, free - prices[i]);
+            free = Math.max(free, hold + prices[i] - fee);
+        }
+        return free;
     }
 }
