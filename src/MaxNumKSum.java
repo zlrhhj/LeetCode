7af86520ea5e20271e39612afcd705cdb77d9aa5
Liru Zhao <zlrhhj@hotmail.com>
2025-01-20 19:28:32 -0800

    No.1679 Attempt #2 using two pointers

--- src/MaxNumKSum.java
+++ src/MaxNumKSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +13,25 @@ public class MaxNumKSum {
                 map.put(k - nums[i], map.get(k - nums[i]) - 1);
             } else {
                 map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+        }
+        return result;
+    }
+    //Attempt #2 using two pointers
+    public int maxOperations2(int nums[], int k) {
+        int result = 0;
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+            if( sum == k ) {
+                result++;
+                right--;
+                left++;
+            } else if( sum > k ) {
+                right--;
+            } else {
+                left++;
             }
         }
         return result;
