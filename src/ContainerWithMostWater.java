9f20b70d3b2b6c213e493cd9d535a74c7c716b0a
Liru Zhao <zlrhhj@hotmail.com>
2025-01-20 18:52:07 -0800

    NO.11 container with most water

---
+++ src/ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0, right = height.length - 1;
+        while(left < right) {
+            max = Math.max(max, Math.min(height[left], height[right]) * right - left);
+            if(height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return max;
+    }
+}
