9be4011d90ac47aa0a7683be8bfe5d08c190d4c5
Liru Zhao <zlrhhj@hotmail.com>
2025-01-22 11:42:15 -0800

    NO.2095 Attempt #2

--- src/DeleteMiddleNode.java
+++ src/DeleteMiddleNode.java
@@ -10,4 +10,22 @@ public class DeleteMiddleNode {
         slow.next = slow.next.next;
         return head;
     }
+    //Atempt #2 using 2 passses
+    public ListNode deleteMiddle( ListNode head ) {
+        if( head == null || head.next == null ) return null;
+        int count = 0;
+        ListNode current = head;
+        if( current != null ) {
+            count++;
+            current = current.next;
+        }
+        count = count / 2;
+        current = head;
+        while( count > 1 ) {
+            current = current.next;
+            count--;
+        }
+        current.next = current.next.next;
+        return head;
+    }
 }
