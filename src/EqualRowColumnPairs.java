268ba356de289ea8d364e2ca332d7a73ff13f53e
Liru Zhao <zlrhhj@hotmail.com>
2025-01-21 16:47:38 -0800

    No.2352, Attempt #2 using trie

--- src/EqualRowColumnPairs.java
+++ src/EqualRowColumnPairs.java
@@ -1,7 +1,42 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+class TrieNode {
+    int count;
+    Map<Integer, TrieNode> children;
+    public TrieNode() {
+        this.count = 0;
+        this.children = new HashMap<>();
+    }
+}
+class Trie {
+    TrieNode trie;
+    public Trie() {
+        this.trie = new TrieNode();
+    }
+    public void insert(int[] array) {
+        TrieNode myTrie = this.trie;
+        for (int a : array) {
+            if(!myTrie.children.containsKey(a)) {
+                myTrie.children.put(a,new TrieNode());
+            }
+            myTrie = myTrie.children.get(a);
+        }
+        myTrie.count += 1;
+    }
 
+    public int search(int[] array) {
+        TrieNode myTrie = this.trie;
+        for (int a : array ) {
+            if(myTrie.children.containsKey(a)) {
+                myTrie = myTrie.children.get(a);
+            } else {
+                return 0;
+            }
+        }
+        return myTrie.count;
+    }
+}
 public class EqualRowColumnPairs {
     public int equalPairs(int[][] grid) {
         int result = 0;
@@ -21,5 +56,21 @@ public class EqualRowColumnPairs {
             }
         }
         return result;
+    }
+    public int equalPairs1 (int[][] grid) {
+        Trie trie = new Trie();
+        int count = 0;
+        for ( int[] row : grid ) {
+            trie.insert(row);
+        }
+
+        for ( int i = 0; i < grid.length; i++ ) {
+            int[] column = new int[grid.length];
+            for ( int r = 0; r < grid.length; r++ ) {
+                column[r] = grid[r][i];
+            }
+            count += trie.search( column );
+        }
+        return count;
     }
 }
