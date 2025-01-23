5d21d8d6e51d793f1e187c37c7db6b3f25bc13cf
Liru Zhao <zlrhhj@hotmail.com>
2025-01-22 22:21:06 -0800

    Attemp #1 using hashmap and hashset

--- src/UniqueNumberOccurences.java
+++ src/UniqueNumberOccurences.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class UniqueNumberOccurences {
+    public boolean uniqueOccurences(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+
+        for(int value : map.values()) {
+           if(!set.add(value)) return false;
+        }
+        return true;
+    }
 }
