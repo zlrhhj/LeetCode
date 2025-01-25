db950b861131e371c75e186b2c6bc31c66edf47b
Liru Zhao <zlrhhj@hotmail.com>
2025-01-24 22:56:46 -0800

    No.841 - Attempt #2 using queue

--- src/KeysAndRooms.java
+++ src/KeysAndRooms.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class KeysAndRooms {
     public void dfs(int key, List<List<Integer>> rooms, Set<Integer> visited) {
@@ -14,6 +12,23 @@ public class KeysAndRooms {
         Set<Integer> visited = new HashSet<>();
         visited.add(0);
         dfs(0, rooms, visited);
+        return visited.size() == rooms.size() ? true : false;
+    }
+
+    public boolean canVisitAllRooms1 (List<List<Integer>> rooms) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        Set<Integer> visited = new HashSet<>();
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            for(int i = 0; i < len; i++) {
+                for(int key : rooms.get(queue.remove())) {
+                    if(visited.add(key)) {
+                        queue.add(key);
+                    }
+                }
+            }
+        }
         return visited.size() == rooms.size() ? true : false;
     }
 }
