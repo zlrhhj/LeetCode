c0475af5437a1c3e691257d0f66444c2056f38d6
Liru Zhao <zlrhhj@hotmail.com>
2025-01-25 22:03:58 -0800

    NO.208 Implement trie(Prefix Tree)

---
+++ src/Trie.java
@@ -0,0 +1,51 @@
+import com.sun.source.tree.Tree;
+
+class TrieNode {
+    boolean isWord;
+    TrieNode[] children;
+    public TrieNode() {
+        isWord = false;
+        children = new TrieNode[26];
+    }
+}
+public class Trie {
+    TrieNode root;
+    public Trie() {
+        root = new TrieNode();
+    }
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+
+    public boolean startWith (String prefix) {
+        TrieNode node = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if(node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
