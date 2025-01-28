d154c3edffb3180e8f44e8a3c1fee6294156916d
Liru Zhao <zlrhhj@hotmail.com>
2025-01-28 12:47:22 -0800

    NO.2336 smallest number in infinite set

---
+++ src/SmallestInfiniteSet.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+public class SmallestInfiniteSet {
+    private PriorityQueue<Integer> queue;
+    private int current;
+    public SmallestInfiniteSet() {
+        queue = new PriorityQueue<>();
+        current = 1;
+    }
+    public int popSmallest() {
+        int result = current;
+        if(!queue.isEmpty() && queue.peek() < current) {
+            result = queue.poll();
+        } else {
+            current++;
+        }
+        while(!queue.isEmpty() && queue.peek() == result) {
+            queue.poll();
+        }
+        return result;
+    }
+    public void addBack(int num) {
+        queue.add(num);
+    }
+}
