a3030967a27af38757056085a99e9e72d9414e54
Liru Zhao <zlrhhj@hotmail.com>
2025-02-05 10:38:14 -0800

    No.42 Trapping Rain Water

--- src/TrappingRainWater.java
+++ src/TrappingRainWater.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class TrappingRainWater {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftHigh = new int[n];
+        int[] rightHigh = new int[n];
+        for(int i = 1; i < n; i++) {
+            leftHigh[i] = Math.max(leftHigh[i - 1], height[i - 1]);
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            rightHigh[i] = Math.max(rightHigh[i + 1], height[i + 1]);
+        }
+        int water = 0;
+        for(int i = 1; i < n - 1; i++) {
+            int min = Math.min(leftHigh[i], rightHigh[i]);
+            if(height[i] < min) {
+                water += min - height[i];
+            }
+        }
+        return water;
+    }
 }
