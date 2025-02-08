389f668d126fbd2447204c8d3bde8a0e80b11e3e
Liru Zhao <zlrhhj@hotmail.com>
2025-02-08 13:15:56 -0800

    No74. Attempt #2 using binary search

--- src/Search2DMatrix.java
+++ src/Search2DMatrix.java
@@ -9,4 +9,23 @@ public class Search2DMatrix {
         }
         return false;
     }
+    //Attempt #2 using binary search
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+        while(left <= right) {
+            int mid = left + ( right - left ) / 2;
+            int r = mid / n;
+            int c = mid % n;
+            if(matrix[r][c] == target ) {
+                return true;
+            } else if( matrix[r][c] > target ) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
 }
