1b35b40ce2dfaa1640de205b7e35ffea8e751d4d
Liru Zhao <zlrhhj@hotmail.com>
2025-02-15 11:18:40 -0800

    No.973 K Closet Points to Origin ---Attempt #2

--- src/KCloestPoints.java
+++ src/KCloestPoints.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class KCloestPoints {
-    public int[][] kCloest(int[][] points, int k) {
+    //    Attempt #1 : using PriorityQueue;
+    public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0] * a[0] + a[1]*a[1]));
         for(int[] point : points) {
             pq.add(point);
@@ -12,5 +14,14 @@ public class KCloestPoints {
             res[i] = pq.remove();
         }
         return res;
+    }
+    //Attempt #2 just sort points Array
+    public int[][] kClosest2(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> {
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return distA - distB;
+        });
+        return Arrays.copyOf(points, k);
     }
 }
