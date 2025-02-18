9a256b0e962ca2408a497ef1d913fa36c16b254b
Liru Zhao <zlrhhj@hotmail.com>
2025-02-17 16:56:44 -0800

    No.621 task scheduler ---- Attempt #2

--- src/TaskScheduler.java
+++ src/TaskScheduler.java
@@ -32,4 +32,23 @@ public class TaskScheduler {
         }
         return res;
     }
+    //Atempt #2
+    public int leastIntervals2(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for(char task : tasks) {
+            freq[task - 'A']++;
+        }
+        int max = 0;
+        int maxCount = 0;
+        for(int i : freq) {
+            if(i == max) {
+                maxCount++;
+            } else if(i > max) {
+                max = i;
+                maxCount++;
+            }
+        }
+        int res = (max - 1) * (n + 1) + maxCount;
+        return Math.max(res, tasks.length);
+    }
 }
