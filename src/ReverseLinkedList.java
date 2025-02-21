3f9c30c2f7026554c0c66ff3f392f49c881cab78
Liru Zhao <zlrhhj@hotmail.com>
2025-02-21 11:25:44 -0800

    remove the class ListNode

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,15 +1,4 @@
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(){};
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val,ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
+
 public class ReverseLinkedList {
     public ListNode reverserList(ListNode head) {
         if(head == null) {
