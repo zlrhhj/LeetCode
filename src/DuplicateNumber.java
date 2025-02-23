8236789abcfe2b1128c3ba02a56eb9b443a0d7c7
Liru Zhao <zlrhhj@hotmail.com>
2025-02-22 22:11:40 -0800

    No.287 Find duplicate number ---Attempt #2

--- src/DuplicateNumber.java
+++ src/DuplicateNumber.java
@@ -7,7 +7,16 @@ public class DuplicateNumber {
         nums[cur] = cur;
         return store(nums, next);
     }
-    public int findDulicate(int[] nums) {
+    public int findDuplicate(int[] nums) {
         return store(nums, 0);
+    }
+//    Attempt #2 :
+    public int findDuplicate2(int[] nums) {
+        while(nums[0] != nums[nums[0]]) {
+            int temp = nums[nums[0]];
+            nums[nums[0]] = nums[0];
+            nums[0] = temp;
+        }
+        return nums[0];
     }
 }
