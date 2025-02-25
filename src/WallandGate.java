0470b4fcc5944a9a14b898e2b59acabbe3cf466d
Liru Zhao <zlrhhj@hotmail.com>
2025-02-25 14:27:29 -0800

    No286. walls and gates ---- Attempt #2 using BFS

--- src/WallandGate.java
+++ src/WallandGate.java
@@ -1,8 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class WallandGate {
     public void wallsAndGates(int[][] rooms) {
         int m = rooms.length;
         int n = rooms[0].length;
-        int[][] res = new int[m][n];
         boolean change = true;
         while(change) {
             change = false;
@@ -36,6 +38,43 @@ public class WallandGate {
                         }
                     }
                 }
+            }
+        }
+
+    }
+    //Attempt #2 using BFS
+    public void wallsAndGates2(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList();
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 0) {
+                    queue.add(new int[] {i, j});
+                }
+            }
+        }
+        while(!queue.isEmpty()) {
+            int[] curr = queue.remove();
+            int r = curr[0];
+            int c = curr[1];
+            if(r + 1 < m && grid[r + 1][c] == Integer.MAX_VALUE) {
+                queue.add(new int[] {r + 1, c});
+                grid[r + 1][c] = grid[r][c] + 1;
+            }
+            if(r - 1 >= 0 && grid[r - 1][c] == Integer.MAX_VALUE) {
+                queue.add(new int[] {r - 1, c});
+                grid[r - 1][c] = grid[r][c] + 1;
+            }
+            if(c + 1 < n && grid[r][c + 1] == Integer.MAX_VALUE) {
+                queue.add(new int[] {r, c + 1});
+                grid[r][c + 1] = grid[r][c] + 1;
+            }
+            if(c - 1 >= 0 && grid[r][c - 1] == Integer.MAX_VALUE) {
+                queue.add(new int[] {r, c - 1});
+                grid[r][c - 1] = grid[r][c] + 1;
             }
         }
     }
