39ab5dd69eddfdc63ece4dd5ad811dcf2f1b24eb
Liru Zhao <zlrhhj@hotmail.com>
2025-03-04 10:42:02 -0800

    NO.55 jump game ---- Attempt #2 greedy

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -1,4 +1,5 @@
 public class JumpGame {
+    //Attempt #1 dynamic programing
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
@@ -12,5 +13,15 @@ public class JumpGame {
             }
         }
         return dp[0] == 1;
+    }
+    //Attempt #2
+    public boolean canJump2(int[] nums) {
+        int lastPos = nums.length - 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
     }
 }
