0712fde1dbf167897ebecf51866c3439e0349417
Liru Zhao <zlrhhj@hotmail.com>
2025-03-10 13:09:19 -0700

    No253 Meeting Rooms II ---- Attempt #2

--- src/MeetingRoomsII.java
+++ src/MeetingRoomsII.java
@@ -15,4 +15,27 @@ public class MeetingRoomsII {
         }
         return rooms.size();
     }
+    //Attempt #2:
+    public int minMeetingRoomsII(int[][] intervals) {
+        int len = intervals.length;
+        int[] starts = new int[len];
+        int[] ends = new int[len];
+
+        for(int i = 0; i < len; i++ ) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0;
+        int endsPos = 0;
+        for(int i = 0; i < len; i++) {
+            if(starts[i] < ends[endsPos]) {
+                rooms++;
+            }else {
+                endsPos++;
+            }
+        }
+        return rooms;
+    }
 }
