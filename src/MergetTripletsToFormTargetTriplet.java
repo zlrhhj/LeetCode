eed9fb130bb4621a5b54f28926b6c14232a8072f
Liru Zhao <zlrhhj@hotmail.com>
2025-03-12 11:17:27 -0700

    NO 1899 Merge Triplets to Form Target Triplet  --- Attempt #2

--- src/MergetTripletsToFormTargetTriplet.java
+++ src/MergetTripletsToFormTargetTriplet.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class MergetTripletsToFormTargetTriplet {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         int[] initial = new int[3];
@@ -8,6 +11,19 @@ public class MergetTripletsToFormTargetTriplet {
                 initial[2] = Math.max(initial[2], triplets[i][2]);
                 if(initial[0] == target[0] && initial[1] == target[1] && initial[2] == target[2]) {
                     return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean mergeTripletsII(int[][] triplets, int[] target) {
+        Set<Integer> set = new HashSet<>();
+        for(int[] triplet : triplets) {
+            if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) continue;
+            for(int i = 0; i < 3; i++) {
+                if(triplet[i] == target[i]) {
+                    set.add(i);
+                    if(set.size() == 3) return true;
                 }
             }
         }
