f5e5631a57514666c4ce24adab8001e86b9c8c30
Liru Zhao <zlrhhj@hotmail.com>
2025-03-26 10:24:43 -0700

    No.73 Set Matrix Zeros -- Attempt #2 space complexity is O(1)

--- src/SetMatrixZeros.java
+++ src/SetMatrixZeros.java
@@ -26,4 +26,38 @@ public class SetMatrixZeros {
         }
 
     }
+    //Atempt #2 using O(1) space complexity
+    public void setZeroesII(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean isCol = false;
+
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) isCol = true;
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[0][j] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0) {
+            for(int j = 1; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if(isCol) {
+            for(int i = 1; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
